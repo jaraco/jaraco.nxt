cb631bfcb51438f022629ac5e27fd88b96589690
jaraco <none@none>
2008-06-19 01:46:49 +0000

    Completed 100% coverage of Commands, large coverage of responses Refactored to support easier declarative implementation of commands

--- src/jaraco/nxt/messages.py
+++ src/jaraco/nxt/messages.py
@@ -20,15 +20,16 @@ class Message(object):
 	"A raw message to or from the NXT"
 	
 	_expects_reply = False
+	fields = ()
+	structure = ''
 	
 	def __init__(self, payload):
 		self.payload = payload
 		self.parse_payload()
 
 	def parse_payload(self):
-		if hasattr(self, 'fields') and hasattr(self, 'structure'):
-			values = struct.unpack(self.structure, self.payload)
-			map(lambda f,v: setattr(self, f, v), self.fields, self.values)
+		values = struct.unpack(self.structure, self.payload)
+		map(lambda f,v: setattr(self, f, v), self.fields, values)
 
 	def __str__(self):
 		assert len(self) <= 64
@@ -52,12 +53,17 @@ class Command(Message):
 	_expects_reply = False
 	_command_type = CommandTypes.direct
 	
-	def __init__(self, **kwargs):
-		self.set(kwargs)
+	def __init__(self, *args):
+		assert len(args) == len(self.fields)
+		self.set(dict(zip(self.fields, args)))
 		
 	def set(self, dict):
 		for attr, value in dict.items():
 			setattr(self, attr, value)
+		self.validate_settings()
+
+	def validate_settings(self):
+		return
 
 	@property
 	def command_type(self):
@@ -69,20 +75,22 @@ class Command(Message):
 		message = header + self.get_telegram()
 
 	def get_telegram(self):
-		return ''
+		self.validate_settings()
+		values = map(lambda f: getattr(self, f), self.fields)
+		return struct.pack(self.structure, *values)
 
 class StartProgram(Command):
 	command = 0x00
 	
+	fields = ('filename',)
+
 	@staticmethod
 	def validate_filename(filename):
 		filename_pattern = re.compile('\w{1,15}(\.\w{0,3})?')
 		assert filename_pattern.match(filename), 'invalid filename %s' % filename
 		
-	def __init__(self, filename, **kwargs):
-		self.validate_filename(filename)
-		kwargs['filename'] = filename
-		super(StartProgram, self).__init__(**kwargs)
+	def validate_settings(self):
+		self.validate_filename(self.filename)
 		
 	def get_telegram(self):
 		return self.filename + '\x00'
@@ -130,6 +138,11 @@ class SetOutputState(Command):
 	>>> msg = SetOutputState(OutputPort.a)
 	"""
 	command = 0x04
+	fields = (
+		'port', 'set_power', 'mode_byte', 'regulation_mode',
+		'turn_radio', 'run_state', 'tacho_limit',
+		)
+	structure = 'BbBBbBL'
 	
 	def __init__(
 		self,
@@ -167,17 +180,6 @@ class SetOutputState(Command):
 			self.use_regulation & OutputMode.regulated,
 			))
 		return mode_byte
-		
-	def get_telegram(self):
-		return struct.pack('BbBBbBL',
-			self.port,
-			self.set_power,
-			self.mode_byte,
-			self.regulation_mode,
-			self.turn_ratio,
-			self.run_state,
-			self.tacho_limit,
-			)
 
 class SensorType(SpecEnum):
 	no_sensor = 0
@@ -208,19 +210,13 @@ class SensorMode(SpecEnum):
 	
 class SetInputMode(Command):
 	command = 0x5
+	fields = 'port', 'type', 'mode'
+	structure = 'BBB'
 	
-	def __init__(self, port, type, mode):
-		assert port in range(4)
-		assert type in SensorType.values()
-		assert mode in SensorMode.values()
-
-		values = vars()
-		values.pop('self')
-		
-		self.set(values)
-
-	def get_telegram(self):
-		return struct.pack('BBB', self.port, self.type, self.mode)
+	def validate_settings(self):
+		assert self.port in range(4)
+		assert self.type in SensorType.values()
+		assert self.mode in SensorMode.values()
 
 class OutputState(Message):
 	fields = (
@@ -245,13 +241,11 @@ class OutputState(Message):
 class GetOutputState(Command):
 	command = 0x6
 	_expects_reply = OutputState
-	
-	def __init__(self, port):
-		assert port in OutputPort.values()
-		self.port = port
-		
-	def get_telegram(self):
-		return struct.pack('B', self.port)
+	fields = ('port',)
+	structure = 'B'
+
+	def validate_settings(self):
+		assert self.port in OutputPort.values()
 
 class InputValues(Message):
 	fields = (
@@ -261,16 +255,22 @@ class InputValues(Message):
 		)
 	structure = 'BBBBBBHHhh'
 
+class ResetInputScaledValue(Command):
+	command = 0x8
+	fields = ('port',)
+	structure = 'B'
+	
+	def validate_settings(self):
+		assert self.port in InputPort.values()
+
 class GetInputValues(Command):
 	command = 0x7
 	_expects_reply = InputValues
+	fields = ('port',)
+	structure = 'B'
 	
-	def __init__(self, port):
-		assert port in InputPorts.values()
-		self.port = port
-
-	def get_telegram(self):
-		return struct.pack('B', self.port)
+	def validate_settings(self):
+		assert self.port in InputPort.values()
 
 class GetVersion(Command):
 	_expects_reply = True
@@ -280,27 +280,34 @@ class GetInfo(Command):
 	_expects_reply = True
 	command = 0x9B
 	
-class QueryBattery(Command):
-	_expects_reply = True
-	command = 0xB
-
 class BatteryResponse(Message):
+	fields = 'status', 'millivolts'
+	structure = 'BH'
+	
 	def get_voltage(self):
-		millivolts = struct.unpack('H', self.payload[3:5])[0]
-		return millivolts/1000.0
+		return self.millivolts/1000.0
+
+class GetBatteryLevel(Command):
+	_expects_reply = BatteryResponse
+	# _B_attery command is B... coincidence?
+	command = 0xb
 
 class PlayTone(Command):
 	command = 0x3
+	fields = 'frequency', 'duration'
+	structure = '2H'
 	
-	def __init__(self, frequency, duration = 100):
-		assert 200 <= frequency <= 3000
-		self.frequency = frequency
-		self.duration = duration
+	def validate_settings(self):
+		assert 200 <= self.frequency <= 3000
 
-	def get_telegram(self):
-		return struct.pack('2H', self.frequency, self.duration)
+	def __init__(self, frequency, duration = 100):
+		values = vars()
+		values.pop('self')
+		self.set(values)
 
 class CurrentProgramName(Message):
+	fields = 'status', 'filename'
+	
 	def parse_values(self):
 		self.status = self.payload[0]
 		self.filename = self.payload[1:]
@@ -320,20 +327,114 @@ class KeepAlive(Command):
 
 class MessageWrite(Command):
 	command = 0x9
-	
+	fields = 'box', 'message_len', 'Zmessage'
+
 	def __init__(self, message, box_number=1):
-		assert 0 < box_number <= 10, 'invalid box number %s' % box_number
-		self.box = box_number-1
-		self.message = message
+		values = vars()
+		values.pop('self')
+		self.set(values)
+
+	@property
+	def structure(self):
+		return 'BB%ds' % self.message_len
+
+	@property
+	def Zmessage(self):
+		return ''.join(self.message, '\x00')
+
+	@property
+	def message_len(self):
+		return len(self.message)
+		
+	def validate_settings(self):
+		assert 0 <= self.box < 10, 'invalid box number %(box_number)s' % self.__dict__
+		assert self.message_len <= 0xFF
+
+	@property
+	def box(self):
+		return self.box_number-1
+	
+class ResetMotorPosition(Command):
+	command = 0xa
+	
+	def validate_settings
+	def __init__(self, port, relative=True):
+		assert port in OutputPort.values()
+		self.port = port
+		self.relative = relative
 
 	def get_telegram(self):
-		message = message + '\x00'
-		return struct.pack('BB', self.box, len(message)) + message
+		return struct.pack('BB', self.port, self.relative)
 
-class ReadMailboxMessage(Command):
-	_expects_reply = True
-	command = 0x13
+class StopSoundPlayback(Command):
+	command = 0xc
+
+class LSStatus(Message):
+	fields = ('status', 'num_bytes')
+	structure = 'BB'
+
+class LSGetStatus(Command):
+	command = 0xe
+	_expects_reply = LSStatus
+	fields = ('port',)
+	structure = 'B'
 	
-	def __init__(self, box_number=1):
-		box_index = box_number - 1
-		self.box_indicator = box_index + 10
+	def validate_settings(self):
+		assert self.port in InputPort.values()
+
+class LSWrite(Command):
+	command = 0xf
+	fields = 'port', 'data_length', 'response_length', 'data'
+	
+	@property
+	def structure(self):
+		return 'BBB%ds' % self.data_length
+
+	data_length = property(lambda self: len(self.data))
+	
+	def validate_settings(self):
+		assert self.data_length <= 16
+		assert self.response_length <= 16
+		assert self.port in OutputPort.values()
+	
+	def __init__(self, port, data, response_length = 0):
+		values = vars()
+		values.pop('self')
+		self.set(values)
+
+class StatusResponse(Message):
+	fields = ('status',)
+	structure = 'B'
+
+class LSReadResponse(Message):
+	fields = ('status', 'data')
+	structure = 'B17p'
+
+class LSRead(Command):
+	command = 0x10
+	_expects_reply = LSReadResponse
+	fields = ('port',)
+	structure = 'B'
+	
+	def validate_settings(self):
+		assert self.port in InputPort.values()
+
+class MessageReadResponse(Message):
+	fields = 'status', 'box', 'message'
+	structure = 'BB60p'
+	
+class MessageRead(Command):
+	_expects_reply = MessageReadResponse
+	command = 0x13
+	fields = 'remote_box', 'local_box', 'remove'
+	structure = 'BBB'
+
+	def validate_settings(self):
+		assert 0 <= self.box < 10, 'invalid box number %(box_number)s' % self.__dict__
+		assert self.message_len <= 0xFF
+
+	remote_box = property(lambda self: self.local_box + 0xa)
+	local_box = property(lambda self: self.box_number - 1)
+
+	def __init__(self, box_number=1, remove=True):
+		self.set(box_number=box_number, remove=remove)
