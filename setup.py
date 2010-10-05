9a87a44e60548a3a97e4e18c8a85a88ede4e4316
Jason R. Coombs <jaraco@jaraco.com>
2010-10-05 10:23:19 -0400

    Minor refactoring of setup script

--- setup.py
+++ setup.py
@@ -2,49 +2,49 @@
 
 """ Setup script for building jaraco.nxt distribution
 
-Copyright © 2008-2009 Jason R. Coombs
+Copyright © 2008-2010 Jason R. Coombs
 """
 
-from setuptools import setup, find_packages
-
-__author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
-__version__ = '$Rev$'[6:-2]
-__svnauthor__ = '$Author$'[9:-2]
-__date__ = '$Date$'[7:-2]
+from setuptools import find_packages
 
 name = 'jaraco.nxt'
 
-setup (name = name,
-		version = '1.3.2',
-		description = 'Logo Mindstorms NXT Routines',
-		long_description = open('docs/index.txt').read().strip(),
-		author = 'Jason R. Coombs',
-		author_email = 'jaraco@jaraco.com',
-		url = 'http://pypi.python.org/pypi/'+name,
-		packages = find_packages(exclude=['ez_setup', 'tests', 'examples']),
-		zip_safe=True,
-		namespace_packages = ['jaraco',],
-		license = 'MIT',
-		classifiers = [
-			"Development Status :: 4 - Beta",
-			"Intended Audience :: Developers",
-			"Programming Language :: Python",
+setup_params = dict(
+	name = name,
+	version = '1.3.2',
+	description = 'Logo Mindstorms NXT Routines',
+	long_description = open('docs/index.txt').read().strip(),
+	author = 'Jason R. Coombs',
+	author_email = 'jaraco@jaraco.com',
+	url = 'http://pypi.python.org/pypi/'+name,
+	packages = find_packages(exclude=['ez_setup', 'tests', 'examples']),
+	zip_safe=True,
+	namespace_packages = ['jaraco',],
+	license = 'MIT',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+	],
+	entry_points = dict(
+		console_scripts = [
+			'nxt-control = jaraco.nxt.controller:serve_forever',
 		],
-		entry_points = dict(
-			console_scripts = [
-				'nxt-control = jaraco.nxt.controller:serve_forever',
-			],
-		),
-		install_requires=[
-			'pyserial>=2.2',
-		],
-		extras_require = {
-			'input': 'jaraco.input>=1.1dev',
-		},
-		dependency_links = [
-		],
-		tests_require=[
-			'nose>=0.10',
-		],
-		test_suite = "nose.collector",
-	)
+	),
+	install_requires=[
+		'pyserial>=2.2',
+	],
+	extras_require = {
+		'input': 'jaraco.input>=1.1dev',
+	},
+	dependency_links = [
+	],
+	tests_require=[
+		'nose>=0.10',
+	],
+	test_suite = "nose.collector",
+)
+
+if __name__ == '__main__':
+	from setuptools import setup
+	setup(**setup_params)
